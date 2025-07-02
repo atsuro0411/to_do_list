97d1fafb2fe80bb532f6bdf77424641a0f8ef517
Atsuro <atsumomotarou@gmail.com>
2025-07-02 20:35:39 +0900

    add

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+   def index
+
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+    
+  end
+ 
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def create  
+    @post=Post.new(post_params)
+    
+    @post.save
+    redirect_to("/posts/new")
+  end
+
+  def post_params
+    params.require(:post).permit(:content,:id)
+  end
+end

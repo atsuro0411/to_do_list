9ed1f47b6609d232f8495fdf5a8a9b9effb0fcbf
Atsuro <atsumomotarou@gmail.com>
2025-07-14 13:34:49 +0900

    締切日時の追加

--- db/migrate/20250701054308_create_posts.rb
+++ db/migrate/20250701054308_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[8.0]
   def change
     create_table :posts do |t|
-      t.string :content
-
+      t.string :content, null: false
+      t.boolean :is_completed, default: false
+      t.datetime :due_date, null: false
       t.timestamps
     end
   end
